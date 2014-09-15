db561a62baa454c31959265c9fdc2a3b965c5d39
Alberto Rubio <arubio@elasticbox.com>
2014-09-16 00:47:47 +0200

    Fix error in URL

--- src/main/webapp/WEB-INF/static/files/configurator.js
+++ src/main/webapp/WEB-INF/static/files/configurator.js
@@ -8,7 +8,7 @@ toyotaApp.controller('ConfiguratorCtrl', function($scope, $http) {
         price: 0
     };
 
-    $http.get('/car/3')
+    $http.get('http://localhost/toyotaDemo/car/3')
         .success(function(data) {
             $scope.loaded = true;
             $scope.car = data;
